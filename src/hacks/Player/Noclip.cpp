d0b12fc1c6abd48bc91513240d7b462e8d4324a2
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-19 14:52:51 +0300

    Update Noclip.cpp

--- src/hacks/Player/Noclip.cpp
+++ src/hacks/Player/Noclip.cpp
@@ -4,6 +4,7 @@
 using namespace geode::prelude;
 
 class $modify(NoclipPlayer, PlayerObject) {
+    auto value = Mod::get()->getSettingValue<bool>("bool-setting-example");
     void pushButton(PlayerButton btn) {
         if (Mod::get()->getSettingValue<bool>("noclip")) {
             this->m_isDead = false; // The actual mod
