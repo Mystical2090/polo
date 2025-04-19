bada01fc6936d7a2d1997ea1e1631b2b60dd8d50
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-19 13:17:29 +0300

    Update Noclip.cpp

--- src/hacks/Player/Noclip.cpp
+++ src/hacks/Player/Noclip.cpp
@@ -5,7 +5,7 @@ using namespace geode::prelude;
 
 class $modify(NoclipPlayer, PlayerObject) {
     void collided(cocos2d::CCObject* obj) {
-        if (Mod::get()->getSettingValue<bool>("noclip")) {
+        if (Mod::get()->getSettingValue<bool>("enable-noclip")) {
             // dont die when die
             return;
         }
