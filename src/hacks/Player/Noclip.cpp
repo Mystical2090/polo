765ced8f44e42a2597281902a400bfe315416214
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-19 13:02:02 +0300

    Update Noclip.cpp

--- src/hacks/Player/Noclip.cpp
+++ src/hacks/Player/Noclip.cpp
@@ -6,8 +6,8 @@ using namespace geode::prelude;
 class $modify(NoclipPlayer, PlayerObject) {
     bool checkCollisions(CCRect rect) {
         if (Mod::get()->getSettingValue<bool>("enable-noclip")) {
-            return false; // skip collision
+            return false; // dis makes you not die 
         }
-        return PlayerObject::checkCollisions(rect); // original behavior
+        return PlayerObject::checkCollisions(rect); // dis makes you die
     }
 };
