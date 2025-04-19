418bf3b7ab15872c7f671526a1f696248ab24dba
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-19 18:19:47 +0300

    Update Noclip.cpp

--- src/hacks/Player/Noclip.cpp
+++ src/hacks/Player/Noclip.cpp
@@ -3,15 +3,12 @@
 
 using namespace geode::prelude;
 
-// Only apply this mod if the setting is enabled
-$execute {
-    return Mod::get()->getSettingValue<bool>("enable-noclip");
-}
-
 class $modify(NoclipHack, PlayLayer) {
     void update(float dt) {
-        if (this->m_player1) {
-            this->m_player1->m_isDead = false;
+        if (Mod::get()->getSettingValue<bool>("enable-noclip")) {
+            if (this->m_player1) {
+                this->m_player1->m_isDead = false;
+            }
         }
         PlayLayer::update(dt);
     }
