e6a71fdcaf1433138490df7f61acb8b0ff96428f
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-20 21:24:25 +0300

    Update Speedhack.cpp

--- src/hacks/Global/Speedhack.cpp
+++ src/hacks/Global/Speedhack.cpp
@@ -6,8 +6,11 @@ using namespace geode::prelude;
 
 class $modify(SpeedHack, PlayLayer) {
     void update(float dt) {
-        float speed = Mod::get()->getSettingValue<double>("speed-value");
-        speed = std::clamp(speed, 0.1f, 5.0f); //fixxy fix
-        PlayLayer::update(dt * speed);
+        if (Mod::get()->getSettingValue<bool>("speedhack-bool")) {
+            float speed = Mod::get()->getSettingValue<double>("speed-value");
+            speed = std::clamp(speed, 0.01f, 5.0f);
+            PlayLayer::update(dt * speed);
+        } else {
+            PlayLayer::update(dt);
     }
-};
+}
