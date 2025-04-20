eb5fa7081c1b9e3aff9b228faf72187ce106791d
Mystical :D <peterpanlee2015@gmail.com>
2025-04-20 20:02:28 +0900

    i hate speedhack

--- src/hacks/Global/Speedhack.cpp
+++ src/hacks/Global/Speedhack.cpp
@@ -5,10 +5,18 @@ using namespace geode::prelude;
 class $modify(Speedhack, PlayLayer) {
     void update(float dt) {
         float mult = Mod::get()->getSettingValue<float>("speedhack");
+
+        // logging stuff to see if it worksasjkdc DEV thing
+        log::debug("Running Speedhack: dt = {}, mult = {}", dt, mult);
+
+        // Apply da speedhack to make da work daadsldci
         PlayLayer::update(dt * mult);
     }
 };
 
 void loadGlobalHacks() {
     log::info("Speedhack loaded");
+
+    // Link the class
+    Speedhack::get();
 }
