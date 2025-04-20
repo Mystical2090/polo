2288ec2084358a7871d4adde34204154f409b963
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-20 10:35:50 +0300

    Update Button.cpp

--- src/Button.cpp
+++ src/Button.cpp
@@ -2,6 +2,8 @@
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/ui/GeodeUI.hpp>
 #include <Geode/utils/cocos.hpp>
+#include <Geode/binding/OptionsLayer.hpp> // Include OptionsLayer
+#include <Geode/ext/cocos/CCMenuItemSpriteExtra.hpp> // If CCMenuItemSpriteExtra isn't recognized
 
 using namespace geode::prelude;
 
@@ -15,6 +17,7 @@ void PauseWithImageButton::customSetup() {
     PauseLayer::customSetup();
 
     auto sprite = CCSprite::createWithSpriteFrameName("GJ_optionsBtn_001.png");
+    if (!sprite) return; // Safeguard against missing sprite
 
     auto button = CCMenuItemSpriteExtra::create(
         sprite,
@@ -34,5 +37,11 @@ void PauseWithImageButton::customSetup() {
 }
 
 void PauseWithImageButton::onSettingsButton(cocos2d::CCObject*) {
-    geode::openSettingsPopup(Mod::get(), true); //wowowowow
+    if (auto* options = OptionsLayer::create()) {
+        auto scene = utils::get<cocos2d::CCScene>();
+        if (!scene) return;
+        auto zOrder = scene->getHighestChildZ();
+        scene->addChild(options, zOrder + 1);
+        options->showLayer(false);
+    }
 }
