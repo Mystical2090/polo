4cbb5d54a2c2a8d2276de1afb012c3543da3694c
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-23 13:55:08 +0300

    Update verifyhack.cpp

--- src/hacks/creator/verifyhack.cpp
+++ src/hacks/creator/verifyhack.cpp
@@ -1,24 +1,11 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelEditorLayer.hpp>
-#include <Geode/modify/UploadActionPopup.hpp>
 
 using namespace geode::prelude;
 
-class $modify(UploadActionPopup) {
-    bool init(UploadPopupDelegate* delegate, gd::string str) {
-        if (Mod::get()->getSettingValue<bool>("verify-hack")) {
-            auto level = GameLevelManager::sharedState()->getMainLevel(); // Adjust this to your needs
-            level->m_isVerified = true;
+class $modify(VerifyHackHmmm, EditLevelLayer) {
+        bool init(GJGameLevel* gl) {
+            gl->m_isVerified = true;
+            return EditLevelLayer::init(gl);
         }
-        return UploadActionPopup::init(delegate, str);
-    }
-};
-
-class $modify(LevelEditorLayer) {
-    void onSaveLevel() {
-        if (Mod::get()->getSettingValue<bool>("verify-hack")) {
-            m_level->m_isVerified = true;
-        }
-        base::onSaveLevel(); // this should fix it i hope
-    }
-};
+    };
