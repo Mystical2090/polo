d4c8c0292af0ebbb240cb8ebe778c2bfd15d6311
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-24 16:34:10 +0300

    Update practice-music.cpp

--- src/hacks/misc/practice-music.cpp
+++ src/hacks/misc/practice-music.cpp
@@ -6,9 +6,9 @@
 class $modify(GameStatsManager) {
     bool isItemUnlocked(UnlockType p0, int p1) {
         if (p0 != UnlockType::GJItem || p1 != 17) return GameStatsManager::isItemUnlocked(p0,p1);
-       if (!Mod::get()->getSettingValue<bool>("practice-hack")) {
+       if (Mod::get()->getSettingValue<bool>("practice-hack")) {
             return true;
         } else {
             return GameStatsManager::isItemUnlocked(p0,p1);
         }
-    }
+};
