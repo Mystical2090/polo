c8f93493ae5ecd8f06e6f84d81a2f8c291be2ff8
Mystical :D <peterpanlee2015@gmail.com>
2025-04-24 18:56:20 +0900

    Update speedhack.cpp

--- src/hacks/misc/speedhack.cpp
+++ src/hacks/misc/speedhack.cpp
@@ -1,15 +1,14 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/GameManager.hpp>
-
+#include <Geode/modify/GameManager.hpp> // balls
 using namespace geode::prelude;
 
 class $modify(Speedhack, GameManager) {
     void setGameSpeed(float speed) {
         if (Mod::get()->getSettingValue<bool>("speedhack-bool")) {
             float multiplier = Mod::get()->getSettingValue<float>("speedhack");
-            Speedhack::setGameSpeed$base(speed * multiplier);
+            setGameSpeed$base(speed * multiplier);
         } else {
-            Speedhack::setGameSpeed$base(speed); // i like my cheese drippy bruh
+            setGameSpeed$base(speed);
         }
     }
 };
