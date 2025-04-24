946503327c56482784e693e1fddeb0e9725e7781
Mystical :D <peterpanlee2015@gmail.com>
2025-04-24 22:25:07 +0900

    Update practice-music.cpp

--- src/hacks/misc/practice-music.cpp
+++ src/hacks/misc/practice-music.cpp
@@ -5,9 +5,8 @@
 
 class $modify(GameStatsManager) {
     bool isItemUnlocked(UnlockType p0, int p1) {
-        // did i seriously not see this, all i changed was && to || hA!
         if (p0 != UnlockType::GJItem || p1 != 17) return GameStatsManager::isItemUnlocked(p0,p1);
-        if (Hacks::isHackEnabled("Practice Music") && p1 == 17) {
+       if (!Mod::get()->getSettingValue<bool>("practice-music")) {
             return true;
         } else {
             return GameStatsManager::isItemUnlocked(p0,p1);
