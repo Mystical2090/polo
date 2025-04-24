08736807a906fc5879299096fffcf053b03344bf
Mystical :D <peterpanlee2015@gmail.com>
2025-04-24 22:06:39 +0900

    Update speedhack.cpp

--- src/hacks/misc/speedhack.cpp
+++ src/hacks/misc/speedhack.cpp
@@ -6,9 +6,9 @@ class $modify(Speedhack, GameManager) {
     void setGameSpeed(float speed) {
         if (Mod::get()->getSettingValue<bool>("speedhack-bool")) {
             float multiplier = Mod::get()->getSettingValue<float>("speedhack");
-            setGameSpeed$base(speed * multiplier);
+            setGameSpeed(speed * multiplier);
         } else {
-            setGameSpeed$base(speed);
+            setGameSpeed(speed);
         }
     }
 };
