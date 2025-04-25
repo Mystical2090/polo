84664b05f9a1c1064857554b6aea8a9802410775
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-25 15:22:17 +0300

    Update speedhack.cpp

--- src/hacks/misc/speedhack.cpp
+++ src/hacks/misc/speedhack.cpp
@@ -1,20 +1,21 @@
-#include "speedhack.hpp"
 #include <Geode/Geode.hpp>
+#include <Geode/modify/CCScheduler.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
-bool isSpeedhackBool() {
-    return Mod::get()->getSettingValue<bool>("speedhack-bool");
-}
-
-float getSpeedhack() {
-    return Mod::get()->getSettingValue<float>("speedhack");
-}
-
-float speedhackLogic(float dt) {
-    if (isSpeedhackEnabled()) {
-        float multiplier = getSpeedMultiplier();
-        return std::clamp(dt * multiplier, 0.001f, 0.5f);
+class $modify(CCScheduler) {
+    float getTimeScale() {
+        float originalTimeScale = CCScheduler::getTimeScale();
+        
+        auto speedhackEnabled = Mod::get()->getSettingValue<bool>("enabled");
+        
+        if (speedhackEnabled) {
+            float speedMultiplier = Mod::get()->getSettingValue<float>("speed-value");
+            
+            return originalTimeScale * speedMultiplier;
+        }
+        
+        return originalTimeScale;
     }
-    return dt;
-}
+};
