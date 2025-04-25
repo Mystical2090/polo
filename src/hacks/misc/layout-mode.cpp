fcaf7f45579a4ec66652499595f1d2b0cb88cb22
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-25 12:12:08 +0300

    Update layout-mode.cpp

--- src/hacks/misc/layout-mode.cpp
+++ src/hacks/misc/layout-mode.cpp
@@ -11,8 +11,8 @@ using namespace geode::prelude;
 class $modify(GameObject) {
     void setVisible(bool v) {
         if (Mod::get()->getSettingValue<bool>("layout-mode")) return GameObject::setVisible(v);
-        if (m_objectType == GameObjectType::Decoration && m_objectID != 44 && m_objectID != 38 && m_objectID != 749 && m_objectID != 747) // 44 is checkpoints cus apparebtky thats a quote decoration lmao
-            GameObject::setVisible(false);
+        if (m_objectType == GameObjectType::Decoration && m_objectID != 44 && m_objectID != 38 && m_objectID != 749 && m_objectID != 747); // 44 is checkpoints cus apparebtky thats a quote decoration lmao
+            GameObject::setVisible(false); // mystic pls stop forgeting to put ";"
         } else {
             GameObject::setVisible(v);
         };
