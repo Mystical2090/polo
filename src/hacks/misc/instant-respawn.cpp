2babb58a5ff235d91e1bdf660756e9347c8a8067
Gtxripmxt <gtxripmxt@gmail.com>
2025-04-25 10:10:21 +0300

    Update instant-respawn.cpp

--- src/hacks/misc/instant-respawn.cpp
+++ src/hacks/misc/instant-respawn.cpp
@@ -1,15 +1,56 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/utils/cocos.hpp>
 
 using namespace geode::prelude;
 
 class $modify(PlayLayer) {
-    void destroyPlayer(PlayerObject* player, GameObject* obj) {
-        if (Mod::get()->getSettingValue<bool>("instant-respawn")) {
-            this->resetLevel();
+    bool init(GJGameLevel* level) {
+        if (!PlayLayer::init(level)) {
+            return false;
+        }
+        return true;
+    }
+
+    void resetLevel() {
+        bool instantRespawnEnabled = Mod::get()->getSettingValue<bool>("instant-respawn");
+        
+        if (instantRespawnEnabled && this->m_isDead) {
+            this->m_isDead = false;
+            
+            if (this->m_player1) {
+                this->m_player1->m_isDead = false;
+                this->m_player1->setVisible(true);
+            }
+            if (this->m_player2) {
+                this->m_player2->m_isDead = false;
+                this->m_player2->setVisible(true);
+            }
+            
+            this->m_hasCompletedLevel = false;
+            this->m_bIsDualMode = this->m_bIsDualMode && this->m_levelSettings->m_originalIsDual;
+            this->stopActionByTag(0);
+            
+            PlayLayer::resetLevel();
         } else {
-            PlayLayer::destroyPlayer(player, obj);
+            PlayLayer::resetLevel();
         }
     }
-}; // easiesst hack ever lol
+    
+    void playerKilled(PlayerObject* player) {
+        PlayLayer::playerKilled(player);
+        
+        bool instantRespawnEnabled = Mod::get()->getSettingValue<bool>("instant-respawn");
+        
+        if (instantRespawnEnabled) {
+            this->template runAction(CCSequence::create(
+                CCDelayTime::create(0.01f),
+                CCCallFunc::create(this, callfunc_selector(PlayLayer::resetLevel)),
+                nullptr
+            ));
+        }
+    }
+};
+
+$execute {
+    log::info("Instant Respawn mod has been loaded!");
+}
