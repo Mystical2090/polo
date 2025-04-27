89f848b0a24efa950b24d88d5a92dfdb6f135ff6
Mystical :D <peterpanlee2015@gmail.com>
2025-04-27 13:25:13 +0900

    Update AutoClick.cpp

--- src/hacks/Player/AutoClick.cpp
+++ src/hacks/Player/AutoClick.cpp
@@ -1,11 +1,36 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/CCTouchDispatcher.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+
 using namespace geode::prelude;
 
-class $modify(AutoClickHack, PlayLayer) {
-    void update(float dt) {
-        if (Mod::get()->getSettingValue<bool>("enable-auto-click"))
-            if (this->m_player1) this->m_player1->pushButton(PlayerButton::Jump);
-        PlayLayer::update(dt);
+// Global pointer to track the active PlayLayer
+static PlayLayer* g_activePlayLayer = nullptr;
+
+// Hook PlayLayer to grab the pointer when a level starts
+class $modify(Autoclicker, PlayLayer) {
+public:
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects))
+            return false;
+
+        g_activePlayLayer = this; // Save pointer to active PlayLayer
+        return true;
     }
-}; //not working 
+};
+
+// Hook CCTouchDispatcher to simulate AutoClick
+class $modify(AutoClickTouchDispatcher, CCTouchDispatcher) {
+public:
+    bool touchesBegan(CCSet* touches, CCEvent* event) {
+        bool result = CCTouchDispatcher::touchesBegan(touches, event);
+
+        if (Mod::get()->getSettingValue<bool>("auto-click-enabled")) {
+            if (g_activePlayLayer && g_activePlayLayer->m_player1) {
+                g_activePlayLayer->m_player1->pushButton(PlayerButton::Jump);
+            }
+        }
+
+        return result;
+    }
+};
