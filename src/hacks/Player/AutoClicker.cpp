d2fddb4dbac98dd7af7fe199cc4fb7fc4159dddc
Mystical :D <peterpanlee2015@gmail.com>
2025-04-27 13:30:46 +0900

    Update AutoClicker.cpp

--- src/hacks/Player/AutoClicker.cpp
+++ src/hacks/Player/AutoClicker.cpp
@@ -1,36 +1,32 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCTouchDispatcher.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-
 using namespace geode::prelude;
 
-// Global pointer to track the active PlayLayer
+// abcdefghijklmnopqrst
 static PlayLayer* g_activePlayLayer = nullptr;
 
-// Hook PlayLayer to grab the pointer when a level starts
 class $modify(Autoclicker, PlayLayer) {
 public:
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
 
-        g_activePlayLayer = this; // Save pointer to active PlayLayer
+        g_activePlayLayer = this;
         return true;
     }
 };
 
-// Hook CCTouchDispatcher to simulate AutoClick
+// Hook CCTouchDispatcher to simulate a click
 class $modify(AutoClickTouchDispatcher, CCTouchDispatcher) {
 public:
-    bool touchesBegan(CCSet* touches, CCEvent* event) {
-        bool result = CCTouchDispatcher::touchesBegan(touches, event);
+    void touchesBegan(CCSet* touches, CCEvent* event) override {
+        CCTouchDispatcher::touchesBegan(touches, event); // Just call, no assignment
 
         if (Mod::get()->getSettingValue<bool>("auto-click-enabled")) {
             if (g_activePlayLayer && g_activePlayLayer->m_player1) {
                 g_activePlayLayer->m_player1->pushButton(PlayerButton::Jump);
             }
         }
-
-        return result;
     }
 };
