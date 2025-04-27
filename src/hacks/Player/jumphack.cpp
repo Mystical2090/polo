8088f5ee2861dd3428a30b0545405080739c32e7
Mystical :D <peterpanlee2015@gmail.com>
2025-04-27 17:17:07 +0900

    Update jumphack.cpp

--- src/hacks/Player/jumphack.cpp
+++ src/hacks/Player/jumphack.cpp
@@ -1,6 +1,5 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
-
 using namespace geode::prelude;
 
 class $modify(JumpHackLayer, GJBaseGameLayer) {
@@ -8,24 +7,27 @@ public:
     void update(float dt) override {
         auto mod = Mod::get();
         bool jumpHackEnabled = mod->getSettingValue<bool>("enable-jump-hack");
-
+        
         if (jumpHackEnabled && m_player1) {
             m_player1->m_isOnGround = true;
-            m_player1->m_vehicleGrounded = true;
+            // Remove m_vehicleGrounded as it doesn't exist in PlayerObject
         }
-
+        
         GJBaseGameLayer::update(dt);
-
+        
         if (jumpHackEnabled && m_player1) {
             m_player1->m_isOnGround = true;
-            m_player1->m_vehicleGrounded = true;
+            // Remove m_vehicleGrounded as it doesn't exist in PlayerObject
         }
     }
-
+    
     static void onModify(auto& self) {
         auto hook = self.getHook("GJBaseGameLayer::update");
         Loader::get()->queueInMainThread([hook]() {
-            hook->enable();
+            // Use unwrap() to get the pointer from Result<Hook*>
+            if (hook.isOk()) {
+                hook.unwrap()->enable();
+            }
         });
     }
 };
