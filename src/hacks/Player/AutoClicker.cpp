8224ab2061850280e2863b20526b78d089aceac4
Mystical :D <peterpanlee2015@gmail.com>
2025-04-27 16:10:19 +0900

    Update AutoClicker.cpp

--- src/hacks/Player/AutoClicker.cpp
+++ src/hacks/Player/AutoClicker.cpp
@@ -1,31 +1,32 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/CCTouchDispatcher.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+
 using namespace geode::prelude;
 
-// abcdefghijklmnopqrst
-static PlayLayer* g_activePlayLayer = nullptr;
+static PlayLayer* g_activePlayLayer = nullptr; // my dick
+static double g_lastClickTime = 0.0;
 
-class $modify(Autoclicker, PlayLayer) {
+class $modify(AutoclickerFix, PlayLayer) {
 public:
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
 
         g_activePlayLayer = this;
+        g_lastClickTime = 0.0;
         return true;
     }
-};
 
-// Hook CCTouchDispatcher to simulate a click
-class $modify(AutoClickTouchDispatcher, CCTouchDispatcher) {
-public:
-    void touchesBegan(CCSet* touches, CCEvent* event) override {
-        CCTouchDispatcher::touchesBegan(touches, event); // Just call, no assignment
+    void update(float dt) override {
+        PlayLayer::update(dt);
 
         if (Mod::get()->getSettingValue<bool>("enable-auto-clicker")) {
-            if (g_activePlayLayer && g_activePlayLayer->m_player1) {
-                g_activePlayLayer->m_player1->pushButton(PlayerButton::Jump);
+            double now = utils::time(); // Geode provides utils::time()
+            if (now - g_lastClickTime > 0.05) { // every 50ms = 20 clicks per second
+                if (this->m_player1) {
+                    this->m_player1->pushButton(PlayerButton::Jump);
+                }
+                g_lastClickTime = now;
             }
         }
     }
