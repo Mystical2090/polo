3e5c4621becbecc726f37e6733a8b2b96e66be8d
Mystical :D <peterpanlee2015@gmail.com>
2025-04-28 22:49:45 +0900

    Update AutoClicker.cpp

--- src/hacks/Player/AutoClicker.cpp
+++ src/hacks/Player/AutoClicker.cpp
@@ -7,7 +7,7 @@ class $modify(Autoclicker, PlayLayer) {
         if (Mod::get()->getSettingValue<bool>("enable-auto-clicker")) {
             if (this->m_player1) {
                 // Directly simulate player input
-                this->m_player1->pushButton(0);  // 0 for Jump
+                this->m_player1->pushButton(0);  // 0 for jumps yay balls pennis
                 this->m_player1->releaseButton(0);
             }
         }
