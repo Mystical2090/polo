fd6a8c40d0dee54252f79e11b52781f922deb4cc
Mystical :D <peterpanlee2015@gmail.com>
2025-04-28 18:31:37 +0900

    Update AutoClicker.cpp

--- src/hacks/Player/AutoClicker.cpp
+++ src/hacks/Player/AutoClicker.cpp
@@ -3,60 +3,61 @@
 
 using namespace geode::prelude;
 
-class $modify(AutoClikcer, GJBaseGameLayer) {
+class $modify(AutoClacker, GJBaseGameLayer) {
 public:
-    struct Fields {
-        int frames = 0;
-        int framesHeld = 0;
-        bool isHeld = false;
-    };
+    bool m_autoClickerEnabled = false;
+    int m_frames = 0;
+    int m_framesHeld = 0;
+    bool m_isHeld = false;
+    bool init() {
+        if (!GJBaseGameLayer::init())
+            return false;
+        this->scheduleUpdate();
+        return true;
+    }
 
-    void checkRepellPlayer() override {
-        GJBaseGameLayer::checkRepellPlayer();
-
-        auto& fcont = m_fields.self();
-
-        fcont->frames++;
-
+    void update(float dt) {
+        GJBaseGameLayer::update(dt);
+        
         auto mod = Mod::get();
         int delay = mod->getSettingValue<int>("auto-clicker-delay");
         int holdFor = mod->getSettingValue<int>("auto-clicker-holdfor");
         bool player1Enabled = mod->getSettingValue<bool>("auto-clicker-player1");
         bool player2Enabled = mod->getSettingValue<bool>("auto-clicker-player2");
-
-        if (fcont->frames >= delay) {
-            fcont->frames = 0;
-            fcont->framesHeld = 0;
+        
+        if (!player1Enabled && !player2Enabled)
+            return;
+        
+        m_frames++;
+        
+        if (m_frames >= delay) {
+            m_frames = 0;
+            m_framesHeld = 0;
         }
-
-        if (fcont->frames == 0) {
+        
+        if (m_frames == 0) {
+            // Perform click
             if (player1Enabled)
                 this->handleButton(true, static_cast<int>(PlayerButton::Jump), true);
-
+                
             if (player2Enabled && m_player2 && m_gameState.m_isDualMode)
                 this->handleButton(true, static_cast<int>(PlayerButton::Jump), false);
-
-            fcont->isHeld = true;
+                
+            m_isHeld = true;
         }
-
-        if (fcont->framesHeld >= holdFor) {
+        
+        if (m_framesHeld >= holdFor) {
+            // Release click
             if (player1Enabled)
                 this->handleButton(false, static_cast<int>(PlayerButton::Jump), true);
-
+                
             if (player2Enabled && m_player2 && m_gameState.m_isDualMode)
                 this->handleButton(false, static_cast<int>(PlayerButton::Jump), false);
-
-            fcont->isHeld = false;
+                
+            m_isHeld = false;
         }
-
-        if (fcont->isHeld)
-            fcont->framesHeld++;
-    }
-
-    static void onModify(auto& self) {
-        auto hook = self.getHook("GJBaseGameLayer::checkRepellPlayer");
-        Loader::get()->queueInMainThread([hook]() {
-            hook->enable();
-        });
+        
+        if (m_isHeld)
+            m_framesHeld++;
     }
 };
