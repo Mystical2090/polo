60ad9199396fd8605cf4db71a399ba32c2e5a2d4
Mystical :D <peterpanlee2015@gmail.com>
2025-04-28 22:36:57 +0900

    Update AutoClicker.cpp

--- src/hacks/Player/AutoClicker.cpp
+++ src/hacks/Player/AutoClicker.cpp
@@ -1,63 +1,26 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
-
+#include <Geode/modify/PlayLayer.hpp>
 using namespace geode::prelude;
 
-class $modify(AutoClacker, GJBaseGameLayer) {
-public:
-    bool m_autoClickerEnabled = false;
-    int m_frames = 0;
-    int m_framesHeld = 0;
-    bool m_isHeld = false;
-    bool init() {
-        if (!GJBaseGameLayer::init())
-            return false;
-        this->scheduleUpdate();
-        return true;
-    }
-
+class $modify(AutoClickHack, PlayLayer) {
     void update(float dt) {
-        GJBaseGameLayer::update(dt);
-        
-        auto mod = Mod::get();
-        int delay = mod->getSettingValue<int>("auto-clicker-delay");
-        int holdFor = mod->getSettingValue<int>("auto-clicker-holdfor");
-        bool player1Enabled = mod->getSettingValue<bool>("auto-clicker-player1");
-        bool player2Enabled = mod->getSettingValue<bool>("auto-clicker-player2");
-        
-        if (!player1Enabled && !player2Enabled)
-            return;
-        
-        m_frames++;
-        
-        if (m_frames >= delay) {
-            m_frames = 0;
-            m_framesHeld = 0;
+        if (Mod::get()->getSettingValue<bool>("enable-auto-click")) {
+            if (this->m_player1) {
+                auto playerPos = m_player1->getPosition();
+                
+                auto touch = new cocos2d::CCTouch();
+                touch->autorelease();
+                
+                touch->setTouchInfo(0, playerPos.x, playerPos.y);
+                
+                auto touchSet = cocos2d::CCSet::create();
+                touchSet->addObject(touch);
+                
+                auto dispatcher = cocos2d::CCDirector::sharedDirector()->getTouchDispatcher();
+                dispatcher->touches(touchSet, nullptr, cocos2d::CCTouchDispatcher::kEventTouchBegan);
+                dispatcher->touches(touchSet, nullptr, cocos2d::CCTouchDispatcher::kEventTouchEnded);
+            }
         }
-        
-        if (m_frames == 0) {
-            // Perform click
-            if (player1Enabled)
-                this->handleButton(true, static_cast<int>(PlayerButton::Jump), true);
-                
-            if (player2Enabled && m_player2 && m_gameState.m_isDualMode)
-                this->handleButton(true, static_cast<int>(PlayerButton::Jump), false);
-                
-            m_isHeld = true;
-        }
-        
-        if (m_framesHeld >= holdFor) {
-            // Release click
-            if (player1Enabled)
-                this->handleButton(false, static_cast<int>(PlayerButton::Jump), true);
-                
-            if (player2Enabled && m_player2 && m_gameState.m_isDualMode)
-                this->handleButton(false, static_cast<int>(PlayerButton::Jump), false);
-                
-            m_isHeld = false;
-        }
-        
-        if (m_isHeld)
-            m_framesHeld++;
+        PlayLayer::update(dt);
     }
 };
