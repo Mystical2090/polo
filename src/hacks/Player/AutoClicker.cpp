b45e82639e27d9f3ab4061df37330615fe173cec
Mystical :D <peterpanlee2015@gmail.com>
2025-04-28 22:49:04 +0900

    Update AutoClicker.cpp

--- src/hacks/Player/AutoClicker.cpp
+++ src/hacks/Player/AutoClicker.cpp
@@ -2,23 +2,13 @@
 #include <Geode/modify/PlayLayer.hpp>
 using namespace geode::prelude;
 
-class $modify(AutoClicker, PlayLayer) {
+class $modify(Autoclicker, PlayLayer) {
     void update(float dt) {
         if (Mod::get()->getSettingValue<bool>("enable-auto-click")) {
             if (this->m_player1) {
-                auto playerPos = m_player1->getPosition();
-                
-                auto touch = new cocos2d::CCTouch();
-                touch->autorelease();
-                
-                touch->setTouchInfo(0, playerPos.x, playerPos.y);
-                
-                auto touchSet = cocos2d::CCSet::create();
-                touchSet->addObject(touch);
-                
-                auto dispatcher = cocos2d::CCDirector::sharedDirector()->getTouchDispatcher();
-                dispatcher->touches(touchSet, nullptr, 0); // CCTOUCHBEGAN = 0
-                dispatcher->touches(touchSet, nullptr, 2); // CCTOUCHENDED = 2
+                // Directly simulate player input
+                this->m_player1->pushButton(0);  // 0 for Jump
+                this->m_player1->releaseButton(0);
             }
         }
         PlayLayer::update(dt);
