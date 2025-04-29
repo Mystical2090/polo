c286841ec2386f80b8f95fd350c76945acbcb03a
Mystical :D <peterpanlee2015@gmail.com>
2025-04-29 18:56:05 +0900

    Update AutoClicker.cpp

--- src/hacks/Player/AutoClicker.cpp
+++ src/hacks/Player/AutoClicker.cpp
@@ -2,14 +2,10 @@
 #include <Geode/modify/PlayLayer.hpp>
 using namespace geode::prelude;
 
-class $modify(AutoClickerlayer, PlayLayer) {
+class $modify(AutoClickHack, PlayLayer) {
     void update(float dt) {
-        if (Mod::get()->getSettingValue<bool>("enable-auto-clicker")) {
-            if (this->m_player1) {
-                this->m_player1->pushButton(PlayerButton::Jump);
-                this->m_player1->releaseButton(PlayerButton::Jump);
-            }
-        }
+        if (Mod::get()->getSettingValue<bool>("enable-auto-clicker"))
+            if (this->m_player1) this->pushButton(0, true);
         PlayLayer::update(dt);
     }
 };
