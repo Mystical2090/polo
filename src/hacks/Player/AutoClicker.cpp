9acdbe355d1ebc97a2656055406713f45de2a9d5
Mystical :D <peterpanlee2015@gmail.com>
2025-04-29 22:17:49 +0900

    Update AutoClicker.cpp

--- src/hacks/Player/AutoClicker.cpp
+++ src/hacks/Player/AutoClicker.cpp
@@ -1,11 +1,13 @@
+// FLERO CODE BE ON FIRE RN
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+
 using namespace geode::prelude;
 
-class $modify(AutoClickHack, PlayLayer) {
+class $modify(AutoClickLayer, PlayLayer) {
     void update(float dt) {
         if (Mod::get()->getSettingValue<bool>("enable-auto-clicker"))
-            if (this->m_player1) this->handleButton(true, true);
+            if (this->m_player1) this->handleButton(0, true, true);
         PlayLayer::update(dt);
     }
 };
