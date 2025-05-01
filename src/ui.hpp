49b5df8e0f3e59f63092442be9b7e2dffffaf1d5
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-01 12:24:18 +0300

    Update ui.hpp

--- src/ui.hpp
+++ src/ui.hpp
@@ -4,28 +4,24 @@
 
 using namespace geode::prelude;
 
-class $modify(MyLayer, PoloUI) {
+class $modify(MyPopup) {
     void onMiscButton(cocos2d::CCObject*);
 };
 
-class PoloUI : public geode::Popup<std::string const&> {
+class MyPopup : public geode::Popup<std::string const&> {
 protected:
     bool setup(std::string const& value) override {
         this->setTitle("Polo Mod Menu");
 
-        auto label = CCLabelBMFont::create(value.c_str(), "bigFont.fnt");
-        m_mainLayer->addChildAtPosition(label, Anchor::Center);
-        
     auto miscbtn = ButtonSprite::create("Misc");
-        miscbtn->setScale(0.9f);
-        
-        
+    miscbtn->setScale(0.9f);
+
     auto miscbutton = CCMenuItemSpriteExtra::create(
         miscbtn,
         miscbtn,
         this,
-        menu_selector(MyLayer::onMiscButton)
-);
+        menu_selector(MyPopup::onMiscButton)
+    );
 
     auto winSize = cocos2d::CCDirector::sharedDirector()->getWinSize();
     miscbutton->setPosition({ winSize.width / 2 - 150.f, winSize.height / 2 - 50.f });
@@ -39,8 +35,8 @@ protected:
     }
 
 public:
-    static PoloUI* create(std::string const& text) {
-        auto ret = new MyLayer();
+    static MyPopup* create(std::string const& text) {
+        auto ret = new MyPopup();
         if (ret->initAnchored(240.f, 160.f, text)) {
             ret->autorelease();
             return ret;
