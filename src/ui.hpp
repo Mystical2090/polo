8fd9a6b1e915161e0602e203dd459c0e6b729909
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-02 09:55:03 +0300

    Update ui.hpp

--- src/ui.hpp
+++ src/ui.hpp
@@ -14,6 +14,10 @@ protected:
 
     auto winSize = cocos2d::CCDirector::sharedDirector()->getWinSize();
 
+    auto label = CCLabelBMFont::create("Polo v1.0.0", "bigFont.fnt");
+        label->setScale(0.7f);
+        label->setAnchorPoint({0.5f, 0.5f});
+        label->setPosition({winSize.width - 10.f, 27.f});
     // Misc button
     auto miscbtn = ButtonSprite::create(" Misc  ");
     miscbtn->setScale(0.9f);
