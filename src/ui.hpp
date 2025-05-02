3bd9de8c48d4dd51a551d6b6f596faee30777d69
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-02 13:05:34 +0300

    Update ui.hpp

--- src/ui.hpp
+++ src/ui.hpp
@@ -93,6 +93,101 @@ protected:
         this->m_title->setScale(1.2f); 
     }
 
+#pragma once
+
+#include <Geode/ui/Popup.hpp>
+
+using namespace geode::prelude;
+
+class MyPopup : public geode::Popup<std::string const&> {
+protected:
+    bool setup(std::string const& value) override {
+            this->setTitle("Polo");
+            this->m_title->setScale(1.2f); 
+        
+    auto popupSize = this->getContentSize();
+
+    auto winSize = cocos2d::CCDirector::sharedDirector()->getWinSize();
+// polo version label
+    auto label = CCLabelBMFont::create("Polo v1.0.0", "goldFont.fnt");
+        label->setScale(0.3f);
+        label->setAnchorPoint({1.0f, 0.0f});
+        label->setPosition({ popupSize.width - 10.f, 10.f }); 
+    // Misc button
+    auto miscbtn = ButtonSprite::create(" Misc  ");
+    miscbtn->setScale(0.9f);
+    miscbtn->setContentSize({ 100.f, miscbtn->getContentSize().height });
+    auto miscbutton = CCMenuItemSpriteExtra::create(
+        miscbtn,
+        miscbtn,
+        this,
+        menu_selector(MyPopup::onMiscButton)
+    );
+    miscbutton->setPosition({ popupSize.width * 0.18f, popupSize.height * 0.55f });
+
+    // Player button
+    auto playerbtn = ButtonSprite::create("Player");
+    playerbtn->setScale(0.9f);
+    playerbtn->setContentSize({ 100.f, miscbtn->getContentSize().height });
+    auto playerbutton = CCMenuItemSpriteExtra::create(
+        playerbtn,
+        playerbtn,
+        this,
+        menu_selector(MyPopup::onPlayerButton)
+    );
+    playerbutton->setPosition({ popupSize.width * 0.18f, popupSize.height * 0.67f });
+        // creator button
+    auto creatorbtn = ButtonSprite::create("Creator");
+    creatorbtn->setScale(0.9f);
+    creatorbtn->setContentSize({ 100.f, miscbtn->getContentSize().height });
+    auto creatorbutton = CCMenuItemSpriteExtra::create(
+        creatorbtn,
+        creatorbtn,
+        this,
+        menu_selector(MyPopup::onCreatorButton)
+    );
+    creatorbutton->setPosition({ popupSize.width * 0.18f, popupSize.height * 0.43f });
+        // credits button
+    auto creditsbtn = ButtonSprite::create("Credits");
+    creditsbtn->setScale(0.9f);
+    creditsbtn->setContentSize({ 100.f, miscbtn->getContentSize().height });
+    auto creditsbutton = CCMenuItemSpriteExtra::create(
+        creditsbtn,
+        creditsbtn,
+        this,
+        menu_selector(MyPopup::onCreditsButton)
+    );
+    creditsbutton->setPosition({ popupSize.width * 0.18f, popupSize.height * 0.31f });
+        // mmm yes
+    auto menu = cocos2d::CCMenu::create();
+    menu->addChild(playerbutton);
+    menu->addChild(miscbutton);
+    menu->addChild(creatorbutton);
+    menu->addChild(creditsbutton);
+    menu->addChild(label);
+    menu->setPosition({ 0, 0 });
+
+    this->addChild(menu);
+    return true;
+}
+
+    void onMiscButton(cocos2d::CCObject*) {
+        this->setTitle("Misc");
+        this->m_title->setScale(1.2f); 
+    }
+    void onPlayerButton(cocos2d::CCObject*) {
+        this->setTitle("Player");
+        this->m_title->setScale(1.2f); 
+    }
+    void onCreatorButton(cocos2d::CCObject*) {
+        this->setTitle("Creator");
+        this->m_title->setScale(1.2f); 
+    }
+    void onCreditsButton(cocos2d::CCObject*) {
+        this->setTitle("Credits");
+        this->m_title->setScale(1.2f); 
+    }
+
 public:
     static MyPopup* create(std::string const& text) {
         auto ret = new MyPopup();
