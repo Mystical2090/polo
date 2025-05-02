5a521cb4b34769069b8308c45e34203ff485bc41
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-02 11:36:22 +0300

    Update ui.hpp

--- src/ui.hpp
+++ src/ui.hpp
@@ -93,7 +93,7 @@ protected:
 public:
     static MyPopup* create(std::string const& text) {
         auto ret = new MyPopup();
-        if (ret->initAnchored(440.f, 250.f, text)) {
+        if (ret->initAnchored(440.f, 320.f, text)) {
             ret->autorelease();
             return ret;
         }
