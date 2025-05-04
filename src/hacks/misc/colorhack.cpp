63e1bfce8a2fff21dd59d87442d9758ca104635c
Mystical :D <peterpanlee2015@gmail.com>
2025-05-04 18:07:55 +0900

    Update colorhack.cpp

--- src/hacks/misc/colorhack.cpp
+++ src/hacks/misc/colorhack.cpp
@@ -3,7 +3,7 @@
 
 using namespace geode::prelude;
 
-class $modify(MyMenuLayer, GameManager) {
+class $modify(Layer, GameManager) {
 bool isColorUnlocked(int p0, UnlockType p1) {
          bool result = GameManager::isColorUnlocked(p0, p1);
  
