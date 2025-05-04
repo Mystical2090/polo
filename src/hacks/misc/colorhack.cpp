049ae72766a5e61448f95d22dc94727d790e3870
Mystical :D <peterpanlee2015@gmail.com>
2025-05-04 17:57:58 +0900

    Update colorhack.cpp

--- src/hacks/misc/colorhack.cpp
+++ src/hacks/misc/colorhack.cpp
@@ -1,1 +1,16 @@
-// hold up
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GameManager.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MyMenuLayer, GameManager) {
+bool isColorUnlocked(int p0, UnlockType p1) {
+         bool result = GameManager::isColorUnlocked(p0, p1);
+ 
+         if (Mod::get()->getSettingValue<bool>("color-hack")) {
+             return true;
+         }
+ 
+         return result;
+     }
+ };
