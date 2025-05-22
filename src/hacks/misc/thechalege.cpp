e170ff1a8cfb0f504e607fc6b95bd5f2f4bb04fb
Mystical433 <peterpanlee2015@gmail.com>
2025-05-22 22:15:36 +0900

    Update thechalege.cpp

--- src/hacks/misc/thechalege.cpp
+++ src/hacks/misc/thechalege.cpp
@@ -5,26 +5,22 @@ using namespace geode::prelude;
 
 class $modify(SecretLayer2) {
 public:
-    static void onModify($modify& self) {
+    static void onModify() {
         if (!Mod::get()->getSettingValue<bool>("thechan")) {
             return;
         }
-
-        self.setHookPriority("SecretLayer2::onSecretLevel", 99999999);
+        getHook("SecretLayer2::onSecretLevel")->setPriority(99999999);
     }
 
     void onSecretLevel(cocos2d::CCObject* sender) {
         if (!Mod::get()->getSettingValue<bool>("thechan")) {
             return;
         }
-
         GameManager::get()->m_sceneEnum = 12;
-
         auto scene = CCTransitionFade::create(
             0.5f,
             PlayLayer::scene(GameLevelManager::get()->getMainLevel(3001, false), false, false)
         );
-
         CCDirector::get()->replaceScene(scene);
     }
 };
