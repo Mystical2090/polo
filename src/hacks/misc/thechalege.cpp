2f1d00e6edf2cdaffd02a28a177f1c39b03ad67e
Mystical433 <peterpanlee2015@gmail.com>
2025-05-22 22:13:30 +0900

    Update thechalege.cpp

--- src/hacks/misc/thechalege.cpp
+++ src/hacks/misc/thechalege.cpp
@@ -1,18 +1,16 @@
-#include <Geode/Geode.hpp>
+\#include <Geode/Geode.hpp>
 #include <Geode/modify/SecretLayer2.hpp>
 
 using namespace geode::prelude;
 
 class $modify(SecretLayer2) {
 public:
-    static void onModify() {
+    static void onModify($modify& self) {
         if (!Mod::get()->getSettingValue<bool>("thechan")) {
             return;
         }
 
-        auto& modify = get();
-
-        modify.setHookPriority("SecretLayer2::onSecretLevel", 99999999);
+        self.setHookPriority("SecretLayer2::onSecretLevel", 99999999);
     }
 
     void onSecretLevel(cocos2d::CCObject* sender) {
