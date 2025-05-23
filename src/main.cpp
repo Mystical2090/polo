475133075b4211e9ec4c129b29be936599673b6d
Mystical433 <peterpanlee2015@gmail.com>
2025-05-23 17:31:23 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Geode/Geode.hpp>
 
 void loadPlayerHacks();
-void loadGlobalHacks();
+void loadGlobalHacks(); // hey gtx this file should get deleted tbh!
 // hey mystic this file is f=very useless!
 $on_mod(Loaded) {
     // idk why i did void loadGlobalHacks theres no cpp file but cool ig idk also idgaf
