c925ac78facac83b0bcd4237722bf73a272ec94d
Gtxripmxt <gtxripmxt@gmail.com>
2025-05-28 10:17:41 +0300

    Update speedhack.hpp

--- src/hacks/misc/speedhack.hpp
+++ src/hacks/misc/speedhack.hpp
@@ -1,6 +1,7 @@
 #pragma once 
 // oh mystic why did you make this when you didnt include it anywere so its baisically useless
 // gtx i used too but you tried to fix and removed it and didnt work so i tried to fix and removed the include so yea
+// then this file is basically useless 
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
