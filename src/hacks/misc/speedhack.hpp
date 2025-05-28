8a19d7a8e151b8198b0d957aed42c4d667d3ffbe
Mystical433 <peterpanlee2015@gmail.com>
2025-05-28 16:18:54 +0900

    Update speedhack.hpp

--- src/hacks/misc/speedhack.hpp
+++ src/hacks/misc/speedhack.hpp
@@ -2,6 +2,7 @@
 // oh mystic why did you make this when you didnt include it anywere so its baisically useless
 // gtx i used too but you tried to fix and removed it and didnt work so i tried to fix and removed the include so yea
 // then this file is basically useless 
+// well yeah it is
 #include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
