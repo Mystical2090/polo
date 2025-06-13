66d05084858b23817eb45fc45f3610e74827a2b5
Mystical433 <peterpanlee2015@gmail.com>
2025-06-13 23:33:36 +0900

    Create nowavepulse.cpp

---
+++ src/hacks/misc/nowavepulse.cpp
@@ -0,0 +1,14 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/HardStreak.hpp>
+
+using namespace geode::prelude;
+
+class $modify(HardStreak) {
+    void updateStroke(float p0) {
+        float pulseValue = Mod::get()->getSettingValue<float>("pulse-size");
+
+        m_pulseSize = pulseValue + 0.5f;
+
+        HardStreak::updateStroke(p0);
+    }
+};
