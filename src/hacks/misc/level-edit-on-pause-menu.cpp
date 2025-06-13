33e86ceec7ce0fd0495f4ca8de4ba727ee7b57ff
Mystical433 <peterpanlee2015@gmail.com>
2025-06-13 22:07:53 +0900

    Update level-edit-on-pause-menu.cpp

--- src/hacks/misc/level-edit-on-pause-menu.cpp
+++ src/hacks/misc/level-edit-on-pause-menu.cpp
@@ -16,7 +16,7 @@ class $modify(PauseLayer) {
         PlayLayer::get()->m_level->m_levelType = a;
     }
 
-    void onEdit(cocos2d::CCObject* sender) override {
+    void onEdit(cocos2d::CCObject* sender) {
         auto a = PlayLayer::get()->m_level->m_levelType;
 
         if (Mod::get()->getSettingValue<bool>("level-edit")) {
