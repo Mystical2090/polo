572b9977b8a4c071a290cf8bde05572b8f7bde2c
Mystical433 <peterpanlee2015@gmail.com>
2025-06-14 19:27:29 +0900

    Create safemode.cpp

---
+++ src/hacks/misc/safemode.cpp
@@ -0,0 +1,20 @@
+/* gtxripmxt i will add like some useless stuff includes because im bored
+*/
+#include <Geode/modify/GameStatsManager.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/GameObject.hpp>
+#include <Geode/modify/GJGameLevel.hpp>
+#include <Geode/modify/FMODAudioEngine.hpp>
+#include <Geode/modify/CCScheduler.hpp>
+#include <Geode/modify/CCSprite.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
+#include <Geode/modify/CCApplication.hpp>
+#include <Geode/Geode.hpp>
+
+class $modify(GJGameLevel) {
+    void savePercentage(int p0, bool p1, int p2, int p3, bool p4) {
+        if (Mod::get()->getSettingValue<bool>("safe-mode")) {
+            GJGameLevel::savePercentage(p0, p1, p2, p3, p4);
+        }
+    }
+};
