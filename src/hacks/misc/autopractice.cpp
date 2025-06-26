6f3eaace5dad81746665697e0e76059e677259db
Mystical433 <peterpanlee2015@gmail.com>
2025-06-26 15:36:33 +0900

    I f if this doesn't work I'm using ai
    I hate this

--- src/hacks/misc/autopractice.cpp
+++ src/hacks/misc/autopractice.cpp
@@ -1,8 +1,8 @@
+// if this doesn't work I'm using ai
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
-
 class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
