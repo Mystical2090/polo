73cfdedae0d873867e2fb06a18659d870c96fdc3
Mystical433 <peterpanlee2015@gmail.com>
2025-07-11 16:12:11 +0900

    Update iconhack.cpp

--- src/hacks/misc/iconhack.cpp
+++ src/hacks/misc/iconhack.cpp
@@ -2,10 +2,12 @@
 #include <Geode/modify/GameManager.hpp>
 using namespace geode::prelude;
 
+extern bool colorHackEnabled;
+
 class $modify(MyMenuLayer, GameManager) {
     bool isIconUnlocked(int p0, IconType p1) {
         bool result = GameManager::isIconUnlocked(p0, p1);
-        if (Mod::get()->getSettingValue<bool>("icon-hack")) {
+        if (colorHackEnabled) {
             return true;
         }
         return result;
