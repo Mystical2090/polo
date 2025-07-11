e44cc39965c3d80e082992b27cfc78e05d7113cd
Mystical433 <peterpanlee2015@gmail.com>
2025-07-11 23:17:41 +0900

    Update level-edit-on-pause-menu.cpp

--- src/hacks/misc/level-edit-on-pause-menu.cpp
+++ src/hacks/misc/level-edit-on-pause-menu.cpp
@@ -1,6 +1,5 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PauseLayer.hpp>
-
 using namespace geode::prelude;
 
 extern bool levelEditEnabled;
@@ -8,25 +7,11 @@ extern bool levelEditEnabled;
 class $modify(PauseLayer) {
     void customSetup() override {
         PauseLayer::customSetup();
-
-        auto a = PlayLayer::get()->m_level->m_levelType;
-
+        
+        auto originalLevelType = PlayLayer::get()->m_level->m_levelType;
+        
         if (levelEditEnabled) {
             PlayLayer::get()->m_level->m_levelType = GJLevelType::Editor;
         }
-
-        PlayLayer::get()->m_level->m_levelType = a;
-    }
-
-    void onEdit(cocos2d::CCObject* sender) {
-        auto a = PlayLayer::get()->m_level->m_levelType;
-
-        if (levelEditEnabled) {
-            PlayLayer::get()->m_level->m_levelType = GJLevelType::Editor;
-        }
-
-        PauseLayer::onEdit(sender);
-
-        PlayLayer::get()->m_level->m_levelType = a;
     }
 };
