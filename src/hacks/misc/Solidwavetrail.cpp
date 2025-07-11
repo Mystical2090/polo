ef3f8346e5f4225c666cfad512140aa64353ef96
Mystical433 <peterpanlee2015@gmail.com>
2025-07-11 23:11:14 +0900

    Create Solidwavetrail.cpp

---
+++ src/hacks/misc/Solidwavetrail.cpp
@@ -0,0 +1,17 @@
+#include <Geode/modify/CCDrawNode.hpp>
+using namespace geode::prelude;
+
+extern bool SolidWave
+
+class $modify (CCDrawNode) {
+    bool drawPolygon(CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor)
+    {
+        ccColor4F fill = fillColor;
+        if (SolidWave)
+        {
+            fill = ccc4f(getColor().r / 255.0f, getColor().g / 255.0f, getColor().b / 255.0f, getOpacity() / 255.0f);
+            setBlendFunc({GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA});
+        }
+        return CCDrawNode::drawPolygon(verts, count, fill, borderWidth, borderColor);
+    }
+};
