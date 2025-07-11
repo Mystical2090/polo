c1d11cfeb7f4de14014c989deca9c267be5139e6
Mystical433 <peterpanlee2015@gmail.com>
2025-07-11 16:24:25 +0900

    Update suicide.cpp

--- src/hacks/misc/suicide.cpp
+++ src/hacks/misc/suicide.cpp
@@ -3,6 +3,8 @@
 
 using namespace geode::prelude;
 
+extern bool suicideEnabled;
+
 class $modify(InstantRespawn, PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects)) {
@@ -13,7 +15,7 @@ class $modify(InstantRespawn, PlayLayer) {
     void destroyPlayer(PlayerObject* player, GameObject* obstacle) {
         PlayLayer::destroyPlayer(player, obstacle);
 
-        bool instantRespawnEnabled = Mod::get()->getSettingValue<bool>("instant-respawn");
+        bool instantRespawnEnabled = suicideEnabled;
         if (instantRespawnEnabled) {
             this->resetLevel();
         }
