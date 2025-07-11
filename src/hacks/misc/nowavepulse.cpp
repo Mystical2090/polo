35eae914d7b6264e01a266bb99d6512b6bbafc22
Mystical433 <peterpanlee2015@gmail.com>
2025-07-11 16:17:46 +0900

    Update nowavepulse.cpp

--- src/hacks/misc/nowavepulse.cpp
+++ src/hacks/misc/nowavepulse.cpp
@@ -1,14 +1,12 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/HardStreak.hpp>
-
 using namespace geode::prelude;
+
+extern float noWavePulseValue;
 
 class $modify(HardStreak) {
     void updateStroke(float p0) {
-        float pulseValue = Mod::get()->getSettingValue<float>("wave-pulse-size");
-
-        m_pulseSize = pulseValue + 0.5f;
-
+        m_pulseSize = noWavePulseValue + 0.5f;
         HardStreak::updateStroke(p0);
     }
 };
