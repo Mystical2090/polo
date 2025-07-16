fb5806895e6b7353d61b7b46e74e5f7a5e5cea3b
gtxx9903 <gtxripmxtlol@gmail.com>
2025-07-16 16:27:30 +0300

    more cleanup

--- src/hacks/misc/Solidwavetrail.cpp
+++ src/hacks/misc/Solidwavetrail.cpp
@@ -1,17 +1,18 @@
+#include <Geode/Geode.hpp>
 #include <Geode/modify/CCDrawNode.hpp>
 using namespace geode::prelude;
 
 extern bool SolidWave;
 
-class $modify (CCDrawNode) {
-    bool drawPolygon(CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor)
-    {
+class $modify (HelloMyNameIsGtxx, CCDrawNode) {
+    bool drawPolygon(CCPoint *verts, unsigned int count, const ccColor4F &fillColor, float borderWidth, const ccColor4F &borderColor) {
         ccColor4F fill = fillColor;
-        if (SolidWave)
-        {
+        if (SolidWave) {
             fill = ccc4f(getColor().r / 255.0f, getColor().g / 255.0f, getColor().b / 255.0f, getOpacity() / 255.0f);
             setBlendFunc({GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA});
         }
-        return CCDrawNode::drawPolygon(verts, count, fill, borderWidth, borderColor);
+        else {
+            CCDrawNode::drawPolygon(verts, count, fill, borderWidth, borderColor);
+        }
     }
 };
