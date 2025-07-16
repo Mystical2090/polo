07a4227c77c0386d8a272b3bdab8bdc03239a1e5
gtxx9903 <gtxripmxtlol@gmail.com>
2025-07-16 16:24:38 +0300

    Update everytinghurtsandnosolids.cpp

--- src/hacks/misc/everytinghurtsandnosolids.cpp
+++ src/hacks/misc/everytinghurtsandnosolids.cpp
@@ -1,31 +1,17 @@
-/* gtxripmxt i will add like some useless stuff includes because im bored
-*/
-#include <Geode/modify/GameStatsManager.hpp>
-#include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/GameObject.hpp>
-#include <Geode/modify/PlayerObject.hpp>
-#include <Geode/modify/GJGameLevel.hpp>
-#include <Geode/modify/FMODAudioEngine.hpp>
-#include <Geode/modify/CCScheduler.hpp>
-#include <Geode/modify/CCSprite.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
-#include <Geode/modify/CCApplication.hpp>
 #include <Geode/Geode.hpp>
-
+#include <Geode/modify/PlayerObject.hpp>
 using namespace geode::prelude;
 
 extern bool everythingKillsYouEnabled;
-extern bool noSolidsEnabled;
 
-class $modify(dick, PlayerObject) {
+class $modify(MyPlayerObject, PlayerObject) {
     void collidedWithObject(float p0, GameObject* obj, cocos2d::CCRect p2, bool p3) {
         if (everythingKillsYouEnabled) {
             if (auto playLayer = PlayLayer::get()) {
                 playLayer->destroyPlayer(this, obj);
             }
         }
-
-        if (!noSolidsEnabled) {
+        else {
             PlayerObject::collidedWithObject(p0, obj, p2, p3);
         }
     }
