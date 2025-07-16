1727dd7a02d58510537655d688b8f9d9b8d2a757
gtxx9903 <gtxripmxtlol@gmail.com>
2025-07-16 16:35:49 +0300

    Update basementbypass.cpp

--- src/hacks/misc/basementbypass.cpp
+++ src/hacks/misc/basementbypass.cpp
@@ -1,13 +1,10 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/SecretLayer2.hpp>
-#include <Geode/modify/GameManager.hpp>
-
 using namespace geode::prelude;
 
 extern bool basementBypassEnabled;
 
-class $modify(SecretLayer2) {
-public:
+class $modify(Hello, SecretLayer2) {
     void onDoor(cocos2d::CCObject* sender) {
         auto originalUGV = GameManager::get()->getUGV("4");
 
