70d0591426d47d356446cbfa5bfeef4a2969a1bc
gtxx9903 <gtxripmxtlol@gmail.com>
2025-07-16 16:32:03 +0300

    e

--- src/hacks/misc/autopractice.cpp
+++ src/hacks/misc/autopractice.cpp
@@ -1,19 +1,14 @@
-// if this doesn't work I'm using ai
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
 extern bool autoPracticeEnabled;
 
 using namespace geode::prelude;
-class $modify(PlayLayer) {
+class $modify(Hey, PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        if (!PlayLayer::init(level, useReplay, dontCreateObjects))
-            return false;
-
         if (autoPracticeEnabled) {
             this->togglePracticeMode(true);
         }
-
         return true;
     }
 };
