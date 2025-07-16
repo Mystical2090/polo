1d8ef003bda708d2858b82718da9792827221e20
gtxx9903 <gtxripmxtlol@gmail.com>
2025-07-16 16:39:19 +0300

    Update hidepausebutton.cpp

--- src/hacks/misc/hidepausebutton.cpp
+++ src/hacks/misc/hidepausebutton.cpp
@@ -7,9 +7,6 @@ extern bool hidePauseButtonEnabled;
 
 class $modify(UILayerMod, UILayer) {
     bool init(GJBaseGameLayer* p0) {
-        if (!UILayer::init(p0))
-            return false;
-
         if (hidePauseButtonEnabled) {
             if (auto menu = this->getChildByType<CCMenu>(0)) {
                 if (auto btn = menu->getChildByType<CCMenuItemSpriteExtra>(0)) {
@@ -19,7 +16,6 @@ class $modify(UILayerMod, UILayer) {
                 }
             }
         }
-
         return true;
     }
 };
