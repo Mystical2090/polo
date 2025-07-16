9dd8e91e9fc9b04f0212a96f82d86a05f4671656
gtxx9903 <gtxripmxtlol@gmail.com>
2025-07-16 16:29:59 +0300

    a

--- src/hacks/misc/NoWavepulse.cpp
+++ src/hacks/misc/NoWavepulse.cpp
@@ -1,14 +1,16 @@
+#include <Geode/Geode.hpp>
 #include <Geode/modify/HardStreak.hpp>
 using namespace geode::prelude;
 
 extern bool noWavePulse
 
-class $modify (HardStreak)
-{
-    void updateStroke(float unused)
-    {
-        if (noWavePulse)
+class $modify (Hi, HardStreak) {
+    void updateStroke(float p1) {
+        if (noWavePulse) {
             m_pulseSize = 1;
-        HardStreak::updateStroke(unused);
+        } 
+        else {
+            HardStreak::updateStroke(p1);
+        }
     }
 };
