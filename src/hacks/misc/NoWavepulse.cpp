69d97c5b28101fa26195e93deeda83a056c1570c
qwerty <165166132+qwerty1234567891011121314151617181920@users.noreply.github.com>
2025-07-20 15:39:52 +0900

    Update NoWavepulse.cpp

--- src/hacks/misc/NoWavepulse.cpp
+++ src/hacks/misc/NoWavepulse.cpp
@@ -1,15 +1,19 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/HardStreak.hpp>
+
 using namespace geode::prelude;
 
-extern bool noWavePulse
+extern bool noWavePulse;
 
-class $modify (Hi, HardStreak) {
+$execute {
+    geode::log::info("Wave pulse mod loaded");
+}
+
+class $modify(WavePulseMod, HardStreak) {
     void updateStroke(float p1) {
         if (noWavePulse) {
-            m_pulseSize = 1;
-        } 
-        else {
+            m_pulseSize = 1.0f;
+        } else {
             HardStreak::updateStroke(p1);
         }
     }
