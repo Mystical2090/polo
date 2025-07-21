74945d06c9593ea9e867ec56aed67d76eb15f375
Mystical433 <peterpanlee2015@gmail.com>
2025-07-21 18:16:20 +0900

    Update jumphack.cpp

--- src/hacks/Player/jumphack.cpp
+++ src/hacks/Player/jumphack.cpp
@@ -4,26 +4,20 @@ using namespace geode::prelude;
 
 extern bool jumpHackEnabled;
 
-class $modify(JumpHack, GJBaseGameLayer) {
-public:
-    void update(float dt) override {
-        if (jumpHackEnabled && m_player1) {
-            m_player1->m_isOnGround = true;
+class $modify (GJBaseGameLayer)
+{
+    virtual void update(float dt)
+    {
+        if (jumpHackEnabled) {
+        {
+            if (m_player1)
+                m_player1->m_isOnGround = true;
+
+            if (m_player2)
+                m_player2->m_isOnGround = true;
         }
-        
+
         GJBaseGameLayer::update(dt);
-        
-        if (jumpHackEnabled && m_player1) {
-            m_player1->m_isOnGround = true;
         }
-    }
-    
-    static void onModify(auto& self) {
-        auto hook = self.getHook("GJBaseGameLayer::update");
-        Loader::get()->queueInMainThread([hook]() {
-            if (hook.isOk()) {
-                hook.unwrap()->enable();
-            }
-        });
     }
 };
