b7024152eea79e2101a615ba5ab538bb1b4b76f9
Mystical433 <peterpanlee2015@gmail.com>
2025-07-21 18:21:23 +0900

    for later

---
+++ src/hacks/creator/Disable Copying.cpp
@@ -0,0 +1,19 @@
+#include <Geode/modify/ShareLevelSettingsLayer.hpp>
+
+using namespace geode::prelude;
+/*
+class $modify (ShareLevelSettingsLayer)
+{
+    bool init(GJGameLevel* p0)
+    {
+        if (!ShareLevelSettingsLayer::init(p0))
+            return false;
+
+        if (DisableCopying))
+        {
+        }
+
+        return true;
+    }
+};
+*/
