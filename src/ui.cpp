007cef15645adeed6c2da10048a48d4feb0ff1fe
Mystical433 <peterpanlee2015@gmail.com>
2025-07-28 22:12:15 +0900

    Update ui.cpp\

--- src/ui.cpp
+++ src/ui.cpp
@@ -378,3 +378,16 @@ void SettingsLayer::refreshList() {
     
     m_mainLayer->addChildAtPosition(m_border, Anchor::Right, ccp(-195.f, 0.f));
 }
+
+
+#ifndef GEODE_IS_IOS
+#include <Geode/modify/CCKeyboardDispatcher.hpp>
+class $modify(ImGuiKeybindHook, cocos2d::CCKeyboardDispatcher) {
+    bool dispatchKeyboardMSG(cocos2d::enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
+        if (key == cocos2d::enumKeyCodes::KEY_Tab && isKeyDown) {
+            SettingsLayer::create()->show();
+        }
+        return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
+    }
+};
+#endif
