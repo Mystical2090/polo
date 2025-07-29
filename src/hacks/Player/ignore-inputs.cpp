1e071bfc692d6b4605f10a6254d08e337ccaa9d7
Mystical433 <peterpanlee2015@gmail.com>
2025-07-29 13:35:01 +0900

    tuff?

--- src/hacks/Player/ignore-inputs.cpp
+++ src/hacks/Player/ignore-inputs.cpp
@@ -1,19 +1,16 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayerObject.hpp>
 using namespace geode::prelude;
-
-extern bool ignoreInputsEnabled;
-
 class $modify(PlayerObject) {
     bool pushButton(PlayerButton button) {
-        if (!ignoreInputsEnabled) {
+        if (!ignoreInputsEnabled()) {
             return PlayerObject::pushButton(button);
         }
         return true;
     }
     
     bool releaseButton(PlayerButton button) {
-        if (!ignoreInputsEnabled) {
+        if (!ignoreInputsEnabled()) {
             return PlayerObject::releaseButton(button);
         }
         return true;
