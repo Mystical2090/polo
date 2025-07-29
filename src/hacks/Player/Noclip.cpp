4a79e4b1ef4811bae38546ae0b9ef237cf9095ec
Mystical433 <peterpanlee2015@gmail.com>
2025-07-29 13:34:00 +0900

    Update Noclip.cpp

--- src/hacks/Player/Noclip.cpp
+++ src/hacks/Player/Noclip.cpp
@@ -1,13 +1,11 @@
 #include <Geode/Geode.hpp>
+#include "src/ui.hpp"
 #include <Geode/modify/PlayLayer.hpp>
-
 using namespace geode::prelude;
-
-bool noclipEnabled = false;
 
 class $modify(Noclip, PlayLayer) {
     void destroyPlayer(PlayerObject* player, GameObject* object) {
-        if (!noclipEnabled) {
+        if (!noclipEnabled()) {
             PlayLayer::destroyPlayer(player, object);
         }
     }
